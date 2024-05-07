f182236e1e1ed18720d17ea2e1733fa97e3fb4d0
Emig21 <guerreroemilio001gmail.com>
2024-05-07 15:33:25 -0500

    Esta vez ya esta algo aunque falta

--- formulario/funcion.php
+++ formulario/funcion.php
@@ -2,8 +2,12 @@
 if (empty($_GET["correo"]) || empty($_GET["password1"]) || empty($_GET["password2"])) {
     echo "Ingrese correo y contraseña.";
     echo '<button onclick="history.go(-1);">Regresar</button>';
+    echo '<br>';
+    echo '<img src="img/no autorizo.jpeg" alt="Error">';
 } else {
     echo "¡Todos los campos se han llenado satisfactoriamente!";
     echo '<button onclick="history.go(-1);">Regresar</button>';
+    echo '<br>';
+    echo '<img src="img/autorizo.jpeg" alt="Error">';
 }
 ?>
